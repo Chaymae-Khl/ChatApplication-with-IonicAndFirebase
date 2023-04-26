e55fc58371f11da7d89f2718c134172bf7563800
Chaymae-Khl <khalil.ch19402@gmail.com>
2023-04-26 22:02:32 +0100

    create a home page

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  constructor() { }
+  segment='chats';
+  
+  ngOnInit() {
+  }
+  /**
+   * logout
+   */
+  public logout() {
+    
+  }
+  public onSegmentChanged(event: any) {
+    
+  }
+}
